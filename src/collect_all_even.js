cb3fbb402b96a11d8d795606311bc78dddfdaa65
hutingting119 <1912257350@qq.com>
2016-05-17 19:12:24 +0800

    third

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,27 +1,19 @@
-
-function collect_same_elements(collection_a, object_b) {
-        var newArray =[];
-
-        for(var i=0;i<collection_a.length;i++){
-
-            if (isExist(collection_a[i].key,collection_b["value"])){
-                newArray.push(collection_a[i].key);
-            }
+function collect_same_elements(collection_a, collection_b) {
+    var newArray =[];
+    for(var i=0;i<collection_a.length;i++){
+        if (isExist(collection_a[i].key,collection_b["value"])){
+            newArray.push(collection_a[i].key);
         }
-
-        return newArray;
     }
-
+    return newArray;
+}
 function isExist(element ,collection){
-
-        for(var i=0;i<collection.length;i++){
-
-            if(collection[i]===element){
-                return true;
-            }
+    for(var i=0;i<collection.length;i++){
+        if(collection[i]===element){
+            return true;
         }
-
-        return false;
     }
+    return false;
+}
 
 module.exports = collect_same_elements;
